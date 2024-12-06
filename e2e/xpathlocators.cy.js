48a7a364215b029b928a331ff27738a1517f8156
swathimk07 <swathi@qaoncloud.com>
2024-12-06 16:27:58 +0530

    Delete e2e directory

--- e2e/xpathlocators.cy.js
+++
@@ -1,9 +0,0 @@
-require('cypress-xpath');
-describe('xpathlocators', () =>{
-    it('Xpath', () => {
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //visit - built in comm
-        cy.xpath("//input[@placeholder='Username']").type("Admin")
-        cy.xpath("//input[@placeholder='Password']").type("password")
-        cy.xpath("//button[normalize-space()='Login']").click();
-    })
-});
